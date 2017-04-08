507c514beba5af0efd381b2c26a75df783d50eaa
MartaGoral <maura10@wp.pl>
2017-04-08 11:06:57 +0200

    nastepna zmian2

--- test/test_views.py
+++ test/test_views.py
@@ -14,4 +14,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Marta", "mgs":witaj"}', rv.data)
+        self.assertEquals('{ "imie":"Iza", "mgs":witaj"}', rv.data)
